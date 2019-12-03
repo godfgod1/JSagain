db8ad07ba519eff961a8c07c941c4335a4ae3b35
godfgod1 <54634572+godfgod1@users.noreply.github.com>
2019-12-03 22:24:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-a = 221;
-b = a - 5;
-a = 4;
-console.log(b,a);
+const songInfo = {
+    name:"song",
+    age:33,
+    gender:"Male",
+    handsome:true,
+    favMovies: ["Along the gods", "LOTR", "Oldbody"], 
+    favFood: [
+        {
+            name:"Kimchi", fatty:false}, 
+        { 
+            name:"Cheese burger", fatty: true
+    }
+]
+}
+
+console.log(songInfo.favFood[0].fatty);
